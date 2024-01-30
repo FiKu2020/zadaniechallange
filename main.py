7ec84a97c17d38ba5411ad7944902d540611a2e3
filip k <filip2006kujawa@gmail.com>
2024-01-30 14:18:11 +0100

    chalange 2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-test
+import base64
+dziwny_hash = "WmFzdGFuYXdpYW0gc2llIGtvZ28gdyB0eW0gcm9rdSBvYmxhYw=="
+numerki = "25f9e794323b453885f5181f1b624d0b"
+#sprawdzamy dlugosc 
+print(len(dziwny_hash))
+print(len(numerki))
+## sprawdzamy odkodowane w base64 (import base64) 
+# textbezbase64 = dziwny_hash.encode("ascii")
+# abcd = base64.decode(textbezbase64)
+# odszyfrowany = abcd.decode("ascii")
+# print(odszyfrowany) 
+
