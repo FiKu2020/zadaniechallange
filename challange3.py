6fbf72a83aed2e2f816fcc68987e9adcd54aeeb0
filip k <filip2006kujawa@gmail.com>
2024-02-11 20:43:03 +0100

    moze cos zadziala

--- challange3.py
+++ challange3.py
@@ -1,14 +1,19 @@
 import sqlite3
+import  hashlib
 from hashlib import (md5,sha256,sha512,sha384,sha3_224,sha3_256,sha3_384,sha3_512,blake2b,blake2s,sha1,sha224)
 funkcje =[md5,sha256,sha512,sha384,sha3_224,sha3_256,sha3_384,sha3_512,blake2b,blake2s,sha1,sha224] 
 
 
-def zlam_hash(hash):
+def zlam_hash(pojed_haslo):
     for funkcja in funkcje:
         with open("rockyou.txt", "r") as slownik:
             for linia in slownik:
-        
-    return hash
+                slowo = linia
+                zahash_haslo = slowo.encode().hexdigest()
+                if zahash_haslo == pojed_haslo:
+                    return f"Znaleziono Haslo:{slowo}"
+                else:
+                    return f"Nie znalez hasla"
 
 
 db = sqlite3.connect("users-challange.db").fetchall()
