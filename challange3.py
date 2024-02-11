6dee7d38b34d805a44af27f94a2eb4ab1a72d673
filip k <filip2006kujawa@gmail.com>
2024-02-11 20:06:35 +0100

    a

---
+++ challange3.py
@@ -0,0 +1,20 @@
+import sqlite3
+from hashlib import (md5,sha256,sha512,sha384,sha3_224,sha3_256,sha3_384,sha3_512,blake2b,blake2s,sha1,sha224)
+funkcje =[md5,sha256,sha512,sha384,sha3_224,sha3_256,sha3_384,sha3_512,blake2b,blake2s,sha1,sha224] 
+def zlam_hash(hash):
+    return hash
+db = sqlite3.connect("users-challange.db").fetchall()
+c = db.cursor()
+users = c.execute("Select * from users")
+c.close()
+print(users)
+for u in users:
+    #id
+    print(u[1])
+
+    #imie
+    print(u[1])
+    #email
+    print(u[2])
+    #hash
+    print(u(3))
