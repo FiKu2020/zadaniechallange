81c9d86eb286e09ceedba48b6ca64f31111ea61f
filip k <filip2006kujawa@gmail.com>
2024-02-11 20:13:51 +0100

    dod slownik

--- challange3.py
+++ challange3.py
@@ -1,8 +1,16 @@
 import sqlite3
 from hashlib import (md5,sha256,sha512,sha384,sha3_224,sha3_256,sha3_384,sha3_512,blake2b,blake2s,sha1,sha224)
 funkcje =[md5,sha256,sha512,sha384,sha3_224,sha3_256,sha3_384,sha3_512,blake2b,blake2s,sha1,sha224] 
+
+
 def zlam_hash(hash):
+    for funkcja in funkcje:
+        with open("rockyou.txt", "r") as slownik:
+            for linia in slownik:
+        
     return hash
+
+
 db = sqlite3.connect("users-challange.db").fetchall()
 c = db.cursor()
 users = c.execute("Select * from users")
